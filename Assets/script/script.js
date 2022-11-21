16553978c64a148f2467c21349efd08b747c8c7a
JeenaKozhimukkath <jeenasan@jeenas-mbp.home>
2022-11-21 12:06:40 -0500

    html file location chhanged

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,1 +1,21 @@
 var apiKey = "afbc506061b9a4fcfb29957519d4b8de";
+
+
+var city = "Hamilton"; // My test case was "London"
+var url = "https://api.openweathermap.org/data/2.5/forecast";
+
+$.ajax({
+  url: url, //API Call
+  dataType: "json",
+  type: "GET",
+  data: {
+    q: city,
+    appid: apiKey,
+    units: "metric",
+    cnt: "10"
+  },
+  success: function(data) {
+    console.log('Received data:', data) // For testing
+    
+  }
+});
